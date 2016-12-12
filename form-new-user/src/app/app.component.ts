7280a688890c05242167a08fa2fec8b20cf80439
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-12-12 11:42:50 -0500

    CSS form layout work.

--- form-new-user/src/app/app.component.ts
+++ form-new-user/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="clearfix">
-    <h1 class="round-title">
+    <h1 class="ba br4 p2 pl3 mb3 bg-purple white">
       {{title}}
     </h1>
     <app-form-new-user></app-form-new-user>
-  </div>
-  <div>
-    <app-user-info></app-user-info>
+    <div>
+      <app-user-info></app-user-info>
+    </div>
   </div>
   `,
   styleUrls: ['./app.component.css']
--- form-new-user/src/app/form-new-user/form-new-user.component.ts
+++ form-new-user/src/app/form-new-user/form-new-user.component.ts
@@ -5,25 +5,30 @@ import { NewUserService } from '../new-user.service';
   selector: 'app-form-new-user',
   template: `
     <form #formRef="ngForm">
-      <div>
-        <label for="name">Name:</label>
-        <input type="test" name="name" id="name" ngModel required />
-        <label for="usrname">Surname:</label>
-        <input type="text" id="surname" name="surname" ngModel required />
+      <div class="dt dt--fixed w-50">
+
+        <div class="dt dt--fixed">
+          <label for="name" class="fl w-20 tr pr2 mb2 ">Name:</label>
+          <input type="text" id="name" name="name" ngModel required class="fl w-25 mb2" />
+          <label for="surname" class="fl w-20 tr pr2 mb2">Surname:</label>
+          <input type="text" id="surname" name="surname" ngModel required class="fl w-25 mb2" />
+        </div>
+
+        <div class="dt dt--fixed">
+          <label for="email" class="fl w-20 tr pr2 mb2">Email:</label>
+          <input type="text" id="email" name="email" ngModel required class="fl w-25 mb2" />
+        </div>
+
+        <div class="dt dt--fixed">
+          <label for="password" class="fl w-20 tr pr2 mb2">Password:</label>
+          <input type="password" id="password" name="password" ngModel required class="fl w-25 mb2"/>
+          <label for="surname" class="fl w-20 tr pr2 mb2">&nbsp;</label>
+          <button (click)="onSubmit(formRef.value)" [disabled]="!formRef.valid" class="fl w-25">Create</button>
+        </div>
+
       </div>
-      <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" ngModel required />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" ngModel required />
-      </div>
-        <button (click)="onSubmit(formRef.value)" [disabled]="!formRef.valid">Create</button>
+
     </form>
-    <div>
-      Debug: {{formRef.value | json}}
-    </div>
   `,
   styles: []
 })
