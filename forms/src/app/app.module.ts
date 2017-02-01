14bda40b93a7245f4de4ba20f596cb17cb4c5767
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-02-01 10:18:31 -0500

    Initial forms demo app with login form.

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
