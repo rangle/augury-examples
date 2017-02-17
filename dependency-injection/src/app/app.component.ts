3e67b986114bd9389a77b84752ff6b33267a0f39
Rajinder Yadav <rajindery@hotmail.com>
2017-02-17 13:07:32 -0500

    New build, folder name chyange. (#52)

--- dependency-injection/src/app/app.component.ts
+++ dependency-injection/src/app/app.component.ts
@@ -18,5 +18,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Augury Form Demo';
+  title = 'Augury Dependency Injection Demo';
 }
