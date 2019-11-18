dbd0bd91f07854edffbb165db7983bb73b02f661
Yvonne Allen <yallen011@gmail.com>
2019-11-18 17:27:02 -0500

    finish updating ui

--- routes/src/app/parks/parks.component.ts
+++ routes/src/app/parks/parks.component.ts
@@ -3,18 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parks',
   template: `
+  <div class="ph3">
   <h1>National Parks</h1>
-  <img src="/images/camera.svg" width="20" height="20" align="center">
-  <a [routerLink]="{outlets: { parkit: ['park1']}}" routerLinkActive="active">Park 1</a>
-  <a [routerLink]="{outlets: { parkit: ['park2']}}" routerLinkActive="active">Park 2</a>
-  <a [routerLink]="{outlets: { parkit: ['park3']}}" routerLinkActive="active">Park 3</a>
-  <router-outlet name="parkit"></router-outlet>
+  <a
+   class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+   [routerLink]="{outlets: { parkit: ['park1']}}"
+   routerLinkActive="active">Park 1</a>
+  <a
+  class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+  [routerLink]="{outlets: { parkit: ['park2']}}"
+  routerLinkActive="active">Park 2</a>
+  <a
+  class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+  [routerLink]="{outlets: { parkit: ['park3']}}"
+  routerLinkActive="active">Park 3</a>
+ </div>
+<router-outlet name="parkit"></router-outlet>
   `,
-  styles: [`
-    a.active {
-      background-color: yellow;
-    }
-  `]
+  styles: []
 })
 export class ParksComponent implements OnInit {
 
