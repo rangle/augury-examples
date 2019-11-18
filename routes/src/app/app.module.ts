67f50c6358d39b0f4d89891cd9b147a6cbff772b
Yvonne Allen <yallen011@gmail.com>
2019-11-18 14:57:00 -0500

    moved routing into routing module, updated ui

--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {  HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +9,7 @@ import { ParksComponent } from './parks/parks.component';
 import { ParkOneComponent } from './park-one/park-one.component';
 import { ParkTwoComponent } from './park-two/park-two.component';
 import { ParkThreeComponent } from './park-three/park-three.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,42 +24,7 @@ import { ParkThreeComponent } from './park-three/park-three.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'parks',
-        component: ParksComponent,
-        children: [
-          {
-            path: 'park1',
-            component: ParkOneComponent,
-            outlet: 'parkit'
-          },
-          {
-            path: 'park2',
-            component: ParkTwoComponent,
-            outlet: 'parkit'
-          },
-          {
-            path: 'park3',
-            component: ParkThreeComponent,
-            outlet: 'parkit'
-          }
-        ]
-      },
-      {
-        path: 'cities',
-        loadChildren: () => import('./city-module/city.module').then(m => m.CityModule)
-      }
-    ], {useHash: true})
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- routes/src/app/city-module/city.module.ts
+++ routes/src/app/city-module/city.module.ts
@@ -1,45 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { CityRoutingModule } from './city-routing.module';
 import { CityComponent } from './city.component';
 import { CityOneComponent } from './city-one.component';
 import { CityTwoComponent } from './city-two.component';
 import { CityThreeComponent } from './city-three.component';
 
 @NgModule({
-  imports: [
-    RouterModule.forChild([
-      {
-        path: '',
-        component: CityComponent,
-        children: [
-          {
-            path: 'city1',
-            component: CityOneComponent
-          },
-          {
-            path: 'city2',
-            component: CityTwoComponent
-          },
-          {
-            path: 'city3',
-            component: CityThreeComponent
-          }
-        ],
-      },
-    ])
-  ],
-  exports: [
-    CityComponent,
-    CityOneComponent,
-    CityTwoComponent,
-    CityThreeComponent
-  ],
   declarations: [
     CityComponent,
     CityOneComponent,
     CityTwoComponent,
     CityThreeComponent
+  ],
+  imports: [
+    CityRoutingModule
   ]
 })
 export class CityModule {
