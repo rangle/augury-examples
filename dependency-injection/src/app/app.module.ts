32a83d4ceff915f1e61993aad23fd71cd224ae36
Felix Khoi <feeloor@users.noreply.github.com>
2019-11-20 19:49:13 +0100

    updating dependency injection demo to angular 8 (#87)

--- dependency-injection/src/app/app.module.ts
+++ dependency-injection/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FormNewUserComponent } from './form-new-user/form-new-user.component';
-import { NewUserService  } from './new-user.service';
+import { NewUserService } from './new-user.service';
 import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
@@ -17,9 +16,8 @@ import { UserInfoComponent } from './user-info/user-info.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
   ],
-  providers: [NewUserService ],
+  providers: [NewUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
