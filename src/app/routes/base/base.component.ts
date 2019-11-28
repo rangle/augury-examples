c4706755ea78f98da8762959f7858c6353a2ea4c
Sumit Arora <er.sumitarora@gmail.com>
2019-11-28 08:28:33 -0500

    Adding Prettier

--- src/app/routes/base/base.component.ts
+++ src/app/routes/base/base.component.ts
@@ -1,40 +1,40 @@
-import { Component } from "@angular/core";
-import { NavItem } from "src/app/shared/nav-item";
+import { Component } from '@angular/core';
+import { NavItem } from 'src/app/shared/nav-item';
 
 @Component({
-  selector: "app-routes-root",
-  templateUrl: "./base.component.html",
-  styleUrls: ["./base.component.css"]
+  selector: 'app-routes-root',
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.css']
 })
 export class BaseComponent {
   navModel: NavItem[] = [
     {
-      title: "Augury Routes Demo",
+      title: 'Augury Routes Demo',
       defaultStyle: true,
       headerOnly: false,
-      routerLinkPath: "home"
+      routerLinkPath: 'home'
     },
     {
-      title: "Home",
-      routerLinkPath: "home",
+      title: 'Home',
+      routerLinkPath: 'home',
       defaultStyle: true,
       isNavItem: true
     },
     {
-      title: "Parks",
-      routerLinkPath: "parks",
+      title: 'Parks',
+      routerLinkPath: 'parks',
       defaultStyle: true,
       isNavItem: true
     },
     {
-      title: "Cities",
-      routerLinkPath: "cities",
+      title: 'Cities',
+      routerLinkPath: 'cities',
       defaultStyle: true,
       isNavItem: true
     }
   ];
 
-  title = "Angular Routes Demo";
+  title = 'Angular Routes Demo';
 
   constructor() {}
 }
--- src/app/routes/parks/parks.component.ts
+++ src/app/routes/parks/parks.component.ts
@@ -3,30 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parks',
   template: `
-  <div class="ph3">
-  <h1>National Parks</h1>
-  <a
-   class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
-   [routerLink]="{outlets: { parkit: ['park1']}}"
-   routerLinkActive="active">Park 1</a>
-  <a
-  class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
-  [routerLink]="{outlets: { parkit: ['park2']}}"
-  routerLinkActive="active">Park 2</a>
-  <a
-  class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
-  [routerLink]="{outlets: { parkit: ['park3']}}"
-  routerLinkActive="active">Park 3</a>
- </div>
-<router-outlet name="parkit"></router-outlet>
+    <div class="ph3">
+      <h1>National Parks</h1>
+      <a
+        class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+        [routerLink]="[{ outlets: { parkit: ['park1'] } }]"
+        routerLinkActive="active"
+        >Park 1</a
+      >
+      <a
+        class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+        [routerLink]="[{ outlets: { parkit: ['park2'] } }]"
+        routerLinkActive="active"
+        >Park 2</a
+      >
+      <a
+        class="f6 link dim br1 ph3 pv2 mb2 dib black b-ns btn-bg"
+        [routerLink]="[{ outlets: { parkit: ['park3'] } }]"
+        routerLinkActive="active"
+        >Park 3</a
+      >
+    </div>
+    <router-outlet name="parkit"></router-outlet>
   `,
   styles: []
 })
 export class ParksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
